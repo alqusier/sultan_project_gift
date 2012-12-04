9ab1b820e2c273611d1936a8db68c77cbdfa1ca7
sultan <sultan.eq@gmail.com>
2012-12-04 02:12:14 +0000

    Main updata

---
+++ src/Types_of_Questions/Main_Project.java
@@ -0,0 +1,34 @@
+package Types_of_Questions;
+
+
+	 
+	import java.io.BufferedWriter;
+	import java.io.File;
+	import java.io.FileWriter;
+	import java.io.IOException;
+	import java.io.PrintWriter;
+
+
+	public class Main_Project {
+
+		public static File file;
+		public static PrintWriter out;
+
+		/**
+		 * @param args
+		 * @throws IOException 
+		 */
+		public static void main(String[] args) throws IOException {
+			// TODO Auto-generated method stub
+			// new Gui();
+			new True_False();
+//			new Multiple_Choice();
+//			NewFile();
+
+		}
+
+		
+
+
+
+	}
